4024493689cc477711ec3f2e01ce1cf655cc666a
shinku2d <shinku2d@gmail.com>
2019-09-15 10:13:23 -0600

    Modelos
    Estructuras base creadas y otro elementos

---
+++ docs/sql_script.sql
@@ -0,0 +1,63 @@
+﻿/*
+Created: 11/09/2019
+Modified: 11/09/2019
+Model: MySQL 5.7
+Database: MySQL 5.7
+*/
+
+
+-- Create tables section -------------------------------------------------
+
+-- Table usuarios
+
+CREATE TABLE usuarios
+(
+  id Int NOT NULL AUTO_INCREMENT,
+  correo Varchar(150) NOT NULL,
+  contrasena Varchar(150) NOT NULL,
+  nombre Varchar(150) NOT NULL,
+  imagen Varchar(150) NOT NULL,
+  PRIMARY KEY (id)
+)
+;
+
+-- Table wittes
+
+CREATE TABLE wittes
+(
+  id Int NOT NULL AUTO_INCREMENT,
+  usuario Int NOT NULL,
+  texto Varchar(256) NOT NULL,
+  fecha Date NOT NULL,
+  PRIMARY KEY (id)
+)
+;
+
+-- Table comentarios
+
+CREATE TABLE comentarios
+(
+  id Int NOT NULL AUTO_INCREMENT,
+  usuario Int NOT NULL,
+  witte Int NOT NULL,
+  texto Varchar(256) NOT NULL,
+  fecha Date NOT NULL,
+  PRIMARY KEY (id)
+)
+;
+
+-- Create foreign keys (relationships) section ------------------------------------------------- 
+
+
+ALTER TABLE wittes ADD CONSTRAINT FK_USU_WIT FOREIGN KEY (usuario) REFERENCES usuarios (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+;
+
+
+ALTER TABLE comentarios ADD CONSTRAINT FK_USU_COM FOREIGN KEY (usuario) REFERENCES usuarios (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+;
+
+
+ALTER TABLE comentarios ADD CONSTRAINT FK_WIT_COM FOREIGN KEY (witte) REFERENCES wittes (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+;
+
+
