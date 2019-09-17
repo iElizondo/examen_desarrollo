791b5c8369d46f6c095428c069b29bf1e4a1a221
Isabel Elizondo <isabel.elizondo.meza@est.una.ac.cr>
2019-09-16 19:17:34 -0600

    estructura js y css

---
+++ app/js/login.js
@@ -0,0 +1,16 @@
+var BaseApiUrl;
+
+function buildUrl(service) {
+    return BaseApiUrl + service;
+}
+
+window.onload = function() {
+    var vm = new Vue({
+        el: '#app',
+        data: {},
+        mounted() {},
+        methods: {
+
+        }
+    });
+}
