b22184aab8996e69a6c6d1b314c68a027acd5eb4
Isabel Elizondo <isabel.elizondo.meza@est.una.ac.cr>
2019-09-17 00:08:53 -0600

    error

--- app/js/index.js
+++ app/js/index.js
@@ -13,7 +13,65 @@ Vue.component('witter', {
             edit: false
         }
     },
-    template: ''
+    methods: {
+        editar() {
+            this.edit = true;
+        }
+    },
+    template: `<div class="card p-2">
+    <header class="h-card">
+        <img :src="'../server/uploads/'+witte.usuario.imagen" alt="" class="usuario-card rounded-circle">
+        <p class="fecha">{{witte.fecha}}</p>
+    </header>
+    <div class="c-card">
+        <div v-if="edit">
+            <textarea name="txa_comentario" id="txa_comentario" cols="55" rows="5"></textarea>
+            <button class="btn btn-outline boton btn-action rounded-circle" type="button" id="button-addon2"><i class="fas fa-paper-plane"></i></button>
+        </div>
+        <div v-else>
+            <p class="texto">{{witte.texto}}</p>
+        </div>
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Comentar..." aria-label="Comentar..." aria-describedby="button-addon2">
+            <div class="input-group-append">
+                <button class="btn btn-outline boton btn-principal" type="button" id="button-addon2"><i class="fas fa-paper-plane"></i></button>
+            </div>
+        </div>
+    </div>
+    <div class="l-card mb-2" v-if="!edit">
+        <button @click="editar()" class="btn btn-outline boton btn-action rounded-circle" type="button" id="button-addon2"><i class="fas fa-edit"></i></button>
+        <button class="btn btn-outline boton btn-action rounded-circle" type="button" id="button-addon2"><i class="fas fa-trash-alt"></i></button>
+    </div>
+    <footer>
+        <div class="accordion" id="accordionExample">
+            <div class="card" v-for="contario in witte.comentarios">
+                <div class="card-header ver-cometarios" :id="'comentarios'+witte.id">
+                    <button class="btn btn-link" type="button" data-toggle="collapse" :data-target="'#colap'+witte.id" aria-expanded="true" aria-controls="collapseOne">
+                        <i class="fas fa-eye"> Comentarios</i>
+                </button>
+                </div>
+
+                <div :id="'colap'+witte.id" class="collapse show" :aria-labelledby="'comentarios'+witte.id" data-parent="#accordionExample">
+                    <div class="card-body">
+                        <div class="card p-2">
+                            <header class="h-card">
+                                <img :src="'../server/uploads/'+contario.usuario.imagen" alt="" class="usuario-card rounded-circle">
+                                <p class="fecha">{{contario.fecha}}</p>
+                            </header>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="c-card">
+                                        <p class="texto">{{contario.texto}}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>`
 })
 
 window.onload = function() {
@@ -21,7 +79,6 @@ window.onload = function() {
         el: '#app',
         data: {
             wittes: [],
-            edit: false
         },
         mounted() {
             this.getWittes();
@@ -52,9 +109,6 @@ window.onload = function() {
             },
             insertComentario() {
 
-            },
-            editar() {
-                this.edit = true;
             }
         }
     });
