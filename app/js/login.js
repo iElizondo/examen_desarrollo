13e8e080018de12fba9353300ece6a92fd3adafd
shinku2d <shinku2d@gmail.com>
2019-09-16 20:11:08 -0600

    Login funcional

--- app/js/login.js
+++ app/js/login.js
@@ -1,16 +1,39 @@
-var BaseApiUrl;
+var BaseApiUrl = "http://localhost/examen_desarrollo/server/index.php/api/";
+var ws;
+var vm
 
-function buildUrl(service) {
-    return BaseApiUrl + service;
+function buildUrl(modelo, service) {
+    return BaseApiUrl + modelo + "/" + service;
 }
 
 window.onload = function() {
-    var vm = new Vue({
+    vm = new Vue({
         el: '#app',
-        data: {},
+        data: {
+            correo: "",
+            contrasena: ""
+        },
         mounted() {},
         methods: {
-
+            login(){
+                var url = buildUrl('autenticacion', 'login');
+                var datos = {
+                    correo: this.correo,
+                    contrasena: this.contrasena
+                }
+                axios.post(url, datos).then((response) => {
+                    if(response.data.estado == 'ok'){
+                        location.href="http://localhost/examen_desarrollo/app";
+                    } else if(response.data.estado == 'error'){
+                        alertify.warning(response.data.msg);
+                    }
+                }).catch(error => {
+                    console.log(error);
+                });
+            },
+            registrarse(){
+                location.href="http://localhost/examen_desarrollo/app/register.html";
+            }
         }
     });
 }
